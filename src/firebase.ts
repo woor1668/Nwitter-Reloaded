643ad72ce5dacfbb3ff14672b741b41ba66a3fba
woor1668 <woor16684577@gmail.com>
2024-07-24 11:27:54 +0900

    firebase basic setting

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdw9p4dxz52KAO3dQcDN8g-l2DncgWnrM",
+  authDomain: "nwitter-reloaded-93077.firebaseapp.com",
+  projectId: "nwitter-reloaded-93077",
+  storageBucket: "nwitter-reloaded-93077.appspot.com",
+  messagingSenderId: "644175623146",
+  appId: "1:644175623146:web:5d99402724aa86fe8bb893"
+};
+
+const app = initializeApp(firebaseConfig);
